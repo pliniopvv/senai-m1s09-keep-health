171882f2130c4d429e3bab7538983f5e8131d9f4
Plínio Pvv <gangss2@hotmail.com>
2024-03-23 15:19:02 -0300

    .

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,8 +8,10 @@ export class AuthService {
   users: User[] = [
     {
       id: 0,
-      nome: "Ruth Johns",
-      nascimento: new Date("Tue Oct 24 2023 01:39:09 GMT-0300 (Brasilia Standard Time)"),
+      nome: 'Ruth Johns',
+      nascimento: new Date(
+        'Tue Oct 24 2023 01:39:09 GMT-0300 (Brasilia Standard Time)'
+      ),
       peso: 65,
       altura: 185,
       email: '1234@1234.com',
@@ -18,7 +20,9 @@ export class AuthService {
     {
       id: 1,
       nome: 'Mr. Ian Ruecker',
-      nascimento: new Date("Thu Jun 15 2023 14:57:17 GMT-0300 (Brasilia Standard Time)"),
+      nascimento: new Date(
+        'Thu Jun 15 2023 14:57:17 GMT-0300 (Brasilia Standard Time)'
+      ),
       peso: 75,
       altura: 145,
       email: '4321@4321.com',
@@ -30,19 +34,19 @@ export class AuthService {
 
   verify(email: String, senha: String) {
     let resp = this.users.find((x) => x.email == email && x.senha == senha);
+    if (resp) localStorage.setItem('user', JSON.stringify(resp));
     return !!resp;
   }
 
   reset(email: String) {
-    let resp = this.users.find(x => x.email == email);
-    if (resp)
-      resp.senha = "a1b2c4d4";
+    let resp = this.users.find((x) => x.email == email);
+    if (resp) resp.senha = 'a1b2c4d4';
     this.save();
     return !!resp;
   }
 
   register(user: User) {
-    let nextId = this.users.map(x => x.id).reduce((p,n) => p > n ? p : n);
+    let nextId = this.users.map((x) => x.id).reduce((p, n) => (p > n ? p : n));
     user.id = nextId++;
     this.users.push(user);
     this.save();
